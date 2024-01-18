534b0925c6811e74631564a2a5641ed2af3f54b2
NicoCestari <cestarinicolas.m@gmail.com>
2024-01-18 19:41:35 -0300

    agregado

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ import express  from "express";
 import handlebars from "express-handlebars";
 import __dirname from './utils.js';
 
-const app =express();
+const app = express();
+const users = [
+    {name: 'Bowser', last_name: 'Bros', email: 'bowserbros@gmail.com', age: '150', phone: '2468135756'},
+    {name: 'Mario', last_name: 'Bros', email: 'mbros@gmail.com', age: '50', phone: '1213643678'},
+    {name: 'luiggi', last_name: 'Bros', email: 'lbros@gmail.com', age: '45', phone: '1563213216'},
+    {name: 'Toad', last_name: 'Bros', email: 'tbros@gmail.com', age: '20', phone: '1577654329'},
+    {name: 'Peache', last_name: 'Bros', email: 'ppbros@gmail.com', age: '40', phone: '1509858931'}
+]
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
@@ -11,12 +18,9 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    let testUser = {
-        name: 'Nico',
-        last_name: 'Cestari'
-    };
+    const indice = Math.floor(Math.random() * users.length)
 
-    res.render('index', testUser);
+    res.render('index', users[indice]);
 })
 
 const server = app.listen(8080, () => {
